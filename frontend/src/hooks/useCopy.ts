18d90a8a96c149e45ad678fc2fe646573a6f05d6
ymtech <younes.manjal@gmail.com>
2022-05-11 23:31:20 +0200

    refactor(refactoring): event interfaces, pipes utils & useCopy hooks to reused code

---
+++ frontend/src/hooks/useCopy.ts
@@ -0,0 +1,19 @@
+import { useNotifications } from "@mantine/notifications";
+
+export function useCopy() {
+    /**Mantine UI */
+    const notifications = useNotifications();
+
+    function copyElement(element: string | null | undefined) {
+        /* Mantine Value */
+        notifications.showNotification({
+            id: "Copy",
+            color: "green",
+            message: `This ${typeof element} has been copied : ${element}`,
+            autoClose: 1000,
+        });
+        return navigator.clipboard.writeText(element || "");
+    }
+
+    return [copyElement];
+}
