11f0e4ca2ff0f7d59735bbff3a5df96e72a53174
MarcOreliOoo <albeaurelien@gmail.com>
2022-05-26 17:18:17 +0200

    refactor(proxy factory): implement bytes slice for proxy creation and updatability

--- contracts/CommownSWProxyFactory.sol
+++ contracts/CommownSWProxyFactory.sol
@@ -2,8 +2,9 @@
 pragma solidity 0.8.13;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import "./CommownSW.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./CommownSW.sol";
+import {slice, toUint256, toAddress} from "./BytesUtils.sol";
 
 /// @title Commown Shared Wallet Proxy Factory
 /// @author Aurélien ALBE - Younès MANJAL 😎
@@ -40,40 +41,68 @@ contract CommownSWProxyFactory is Ownable {
 		logic = _contract;
 	}
 
-    /// @notice function called from the front when you create a commown shared wallet
+	/// @notice function called from the front when you create a commown shared wallet
     /// @dev Function to call when you want to create a proxy for owners
-    /// @param _owners list of the owners for the CSW
-    /// @param _confirmationNeeded number of signature required for futures transaction in the CSW
+    /// @param _data bytes data composed of the selector function the proxy will initialize, and parameters : owners' array, number of confirmation and owners
     /// @return address of the proxy
-    function createProxy(address[] memory _owners, uint8 _confirmationNeeded, bytes calldata _data)
+    function createProxy(bytes calldata _data)
         external
         returns (address)
     {
-		uint256 size = _owners.length;
-		require(size <= 255 && size > 0, "_owners.length wrong");
-        require(
-            _confirmationNeeded > 0 && _confirmationNeeded <= size,
-            "invalid confirmation number"
-        );
-
-        //Usage of the ERC1967Proxy
-        //Initialisation of the proxy by calling the initialize function with selector
-        ERC1967Proxy proxy = new ERC1967Proxy(
-            logic,
-            _data
-        );
+		//address[] memory _owners, uint8 _confirmationNeeded, 
+		//Exemple : initial message	
+		//0xeb53a05700000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000002000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb922660000000000000000000000000000000000000000000000000000000000000003000000000000000000000000bda5747bfd65f08deb54cb465eb87d40e51b197e000000000000000000000000dd2fd4581271e230360230f9337d5c0430bf44c00000000000000000000000008626f6940e2eb28930efb4cef49b2d1f2c9c1199
 		
-        //Add the proxy to the global list
-        proxiesList.push(address(proxy));
+		//It can be decomposed in chunks of 32 bytes after the first 4bytes
+		//eb53a057 															> bytes memory _selector  = slice(_data, 0, 4);
+		//0000000000000000000000000000000000000000000000000000000000000060	> position of the array in the bytes
+		//																	  uint256 _uint256positionOwners = toUint256(slice(_params, 0, 32));
 
-        //For each owner add the proxy and increment the proxy number by 1
-        for (uint256 i; i < _owners.length; i++) {
-            commownProxiesPerUser[_owners[i]].push(address(proxy));
-            nbProxiesPerUser[_owners[i]] += 1;
-        }
+		//0000000000000000000000000000000000000000000000000000000000000002  > second param "confirmation needed" as it is a value, it is directly printed
+		//																	  uint8 __uint8confirmationN = uint8(toUint256(slice(_params,32,32)));
 
-        //Emit the event
-        emit ProxyCreated(address(proxy), _owners);
+		//000000000000000000000000f39fd6e51aad88f6f4ce6ab8827279cfffb92266	> third param is the owner() of both contracts
+		//																	  address own = toAddress(slice(_params,64,32),12);
+		
+		//0000000000000000000000000000000000000000000000000000000000000003  > we are at the 96dec = 60hex, here defines the length of the array of owners
+		//																	  uint8 __uint8size = uint8(toUint256(slice(_params,_uint256positionOwners,32)));
+
+		//000000000000000000000000bda5747bfd65f08deb54cb465eb87d40e51b197e  > owners[0]
+		//000000000000000000000000dd2fd4581271e230360230f9337d5c0430bf44c0	> owners[1]
+		//0000000000000000000000008626f6940e2eb28930efb4cef49b2d1f2c9c1199	> owners[2]
+	
+		bytes memory _params = slice(_data, 4, _data.length-4);
+
+		uint256 _uint256positionOwners = toUint256(slice(_params, 0, 32));
+		
+		uint8 __uint8size = uint8(toUint256(slice(_params,_uint256positionOwners,32)));
+		require(__uint8size <= 255 && __uint8size > 0, "_owners.length wrong");
+
+		//Usage of the ERC1967Proxy
+		//Initialisation of the proxy by calling the initialize function with selector
+		ERC1967Proxy proxy = new ERC1967Proxy(
+			logic,
+			_data
+		);
+
+		//Add the proxy to the global list
+		proxiesList.push(address(proxy));
+
+		//Declare an __owners array of size determine by __uint8size visible at _uint256positionOwners
+		address[] memory __owners = new address[](__uint8size);
+		
+		for (uint8 i; i<__uint8size; i++){
+			//Get the owners from the data bytes starting at _uint256positionOwners+32
+			//Because the _uint256positionOwners shows the size of the array
+			__owners[i] = toAddress(slice(_params,_uint256positionOwners+32+(i*32),32),12);
+
+			//For each owner add the proxy and increment the proxy number by 1
+			commownProxiesPerUser[__owners[i]].push(address(proxy));
+			nbProxiesPerUser[__owners[i]] += 1;
+		}
+
+		//Emit the event
+		emit ProxyCreated(address(proxy), __owners);
         return address(proxy);
     }
 }
